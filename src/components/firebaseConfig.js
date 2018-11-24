c8a6434a166e536c573bdecdfa2572696b91d08c
chaowaphan4 <33557489+chaowaphan4@users.noreply.github.com>
2018-11-25 03:29:56 +0700

    master

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDQclFTw6AMk66GOCcPXvh2IjrXVugc4v4",
+    authDomain: "assigment002.firebaseapp.com",
+    databaseURL: "https://assigment002.firebaseio.com",
+    projectId: "assigment002",
+    storageBucket: "assigment002.appspot.com",
+    messagingSenderId: "742536756692"
+};
